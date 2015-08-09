439dd157e65f6a0da01e5f67723064bb08e59879
selman <selmankayrancioglu@gmail.com>
2015-08-10 00:16:54 +0300

    User migration.

--- migrations/2015_08_09_21111439143891_create_users_table.py
+++ migrations/2015_08_09_21111439143891_create_users_table.py
@@ -2,14 +2,16 @@ from orator.migrations import Migration
 
 
 class CreateUsersTable(Migration):
+
     def up(self):
         """
         Run the migrations.
         """
         with self.schema.create('users') as table:
             table.increments('id')
-            table.string('username').unique()
-            table.string('password')
+            table.string('username', 30).unique()
+            table.string('password', 30)
+
             table.timestamps()
 
     def down(self):
