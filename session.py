aec33024083706b49234cee342936374167f7f9c
selman <selmankayrancioglu@gmail.com>
2015-08-11 14:13:32 +0300

    /create 'e requet yollanıp post oluşturulabiliniyor.

--- session.py
+++ session.py
@@ -19,7 +19,10 @@ import models
 db = Orator(app)
 
 
-
+@app.route('/q')
+def q():
+    data = models.User.all().to_dict()
+    return str(data)
 
 @app.route('/')
 def index():
@@ -27,6 +30,32 @@ def index():
         # return
         return "Logged in as %s " % escape(session['username']) + escape(str(session)[1:-1])
     return 'You are not logged in'
+
+
+# {"username":"aa", "title":"bbb", "tag":"dskalda", "announcement":"dskaldaks", "date":"dsjkaldsa"}
+@app.route('/create', methods=['POST'])
+def create():
+    post = request.get_json(force=True)
+
+    tag_id = models.Tag.where('name', post['tag']).first().id
+    user_id = models.User.where('username', post['username']).first().id
+    new_post = models.Post.create(
+        title=post['title'],
+        content=post['announcement'],
+        user_id=user_id,
+        tag_id=tag_id
+    )
+
+    if new_post is not None:
+        response = models.Post.where('title', post['title']).where('tag_id', tag_id).first()
+        response_dict = {'username': response.user.username,
+                         'title': response.title,
+                         'tag': response.tag.name,
+                         'announcement': response.content
+                         }
+        return jsonify(response_dict)
+
+
 
 
 @app.route('/aq', methods=['POST'])
@@ -71,17 +100,23 @@ def loginjson():
         credentials['username'] = str(escape(credentials['username']))  # lemme escape 'em first
         credentials['password'] = str(escape(credentials['password']))  # and let there be string, god said
 
-        if validate(credentials['username'], credentials['password']) != None:  # just kiddin'
+        user = validate(credentials['username'], credentials['password'])
+        if user != None:  # just kiddin'
             session['username'] = credentials['username']
             session['password'] = credentials['password']
+            session['user_id'] = user.id
 
-            return redirect(url_for(('index')))
+            data = {"username": str(escape(session['username'])), "user_id": user.id,
+                    "password": str(escape(session['password']))}
+
+            return jsonify(data)
+
 
     return 'not logged in'
 
 
 def validate(u, p):
-    user = models.User.where('username', u).first()
+    user = models.User.where('username', u).where('password', p).first()
     return user
 
 
@@ -90,7 +125,7 @@ def login():
     if request.method == 'POST':
         return jsonify(request.get_json(force=True))
         # json_string = '{"employees":[{"firstName":"John", "lastName":"Doe"},{"firstName":"Anna", "lastName":"Smith"},{"firstName":"Peter", "lastName":"Jones"}]}'
-        # aa = json.JSONEncoder().encode(json_string) => to python obj
+        # aa = json.JSONEncoder().encode(json_string) =>
         # aa = json.loads(json_string) => to python obj
 
         # data = {"id": str(album.id), "title": album.title}
